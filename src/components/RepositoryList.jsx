879c6571c0221b59efc3568cf69229f68f83bd8b
eusamir <samirandradee@gmail.com>
2021-09-22 16:15:45 -0300

    first commit

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+export function RepositoryList(){
+  return(
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositórios
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositórios
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositórios
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
